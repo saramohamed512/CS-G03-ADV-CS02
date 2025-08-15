4d67c42c6a1804942aa72572b44b5ea62bc679f6
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-08-15 19:47:35 +0300

    Q2:Check if array is palindrome

--- Ass/Program.cs
+++ Ass/Program.cs
@@ -5,19 +5,35 @@
         static void Main(string[] args)
         {
             #region Q1:Count numbers greater than X in array
-            string[] firstLine = Console.ReadLine().Split();
-            int N = int.Parse(firstLine[0]);
-            int Q = int.Parse(firstLine[1]);
+            //string[] firstLine = Console.ReadLine().Split();
+            //int N = int.Parse(firstLine[0]);
+            //int Q = int.Parse(firstLine[1]);
 
-            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            //int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
 
-            for (int i = 0; i < Q; i++)
-            {
-                int X = int.Parse(Console.ReadLine());
-                int count = arr.Count(num => num > X);
-                Console.WriteLine(count);
-            }
+            //for (int i = 0; i < Q; i++)
+            //{
+            //    int X = int.Parse(Console.ReadLine());
+            //    int count = arr.Count(num => num > X);
+            //    Console.WriteLine(count);
+            //}
+            #endregion
+            #region Q2:Check if array is palindrome
+            //int N = int.Parse(Console.ReadLine());
+            //int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            //bool isPalindrome = true;
+            //for (int i = 0; i < N / 2; i++)
+            //{
+            //    if (arr[i] != arr[N - 1 - i])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine(isPalindrome ? "YES" : "NO");
             #endregion
         }
     }
