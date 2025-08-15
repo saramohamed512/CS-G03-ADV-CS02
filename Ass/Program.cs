2b2bbb6ab4fb62865ebe5bc84c52a4b6f25c0d3f
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-08-15 19:41:30 +0300

    Q1

--- Ass/Program.cs
+++ Ass/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1:Count numbers greater than X in array
+            string[] firstLine = Console.ReadLine().Split();
+            int N = int.Parse(firstLine[0]);
+            int Q = int.Parse(firstLine[1]);
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+
+            for (int i = 0; i < Q; i++)
+            {
+                int X = int.Parse(Console.ReadLine());
+                int count = arr.Count(num => num > X);
+                Console.WriteLine(count);
+            }
+            #endregion
         }
     }
 }
