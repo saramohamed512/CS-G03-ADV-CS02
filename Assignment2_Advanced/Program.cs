392381fd70b1d019f7d795d234e53b8fc27736ff
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-08-15 18:27:15 +0300

    Generic collection [list]

--- Assignment2_Advanced/Program.cs
+++ Assignment2_Advanced/Program.cs
@@ -19,31 +19,65 @@ namespace Assignment2_Advanced
             // arrayList.TrimToSize();
 
             //============================================
-            ArrayList arrayListwithCapacity = new ArrayList(5) { 1, 2, 3, "sara", 5 };
+            //ArrayList arrayListwithCapacity = new ArrayList(5) { 1, 2, 3, "sara", 5 };
             //Console.WriteLine($"count:{arrayListwithCapacity.Count} , capacty:{arrayListwithCapacity.Capacity}");
             //arrayListwithCapacity.Add(6);
             //Console.WriteLine($"count:{arrayListwithCapacity.Count} , capacty:{arrayListwithCapacity.Capacity}");
 
-            Console.WriteLine(SumArrayList(arrayListwithCapacity));
-            foreach(object item in arrayListwithCapacity)
-            {
-                Console.WriteLine(item);
-            }
+            //Console.WriteLine(SumArrayList(arrayListwithCapacity));
+            //foreach(object item in arrayListwithCapacity)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
             #endregion
-            #region method to sum nongeneric arraylist
-            static int SumArrayList(ArrayList array)
-            {
-                int sum = 0;
-                if (array != null) {
-                    for (int i=0; i< array.Count; i++) { 
-                        sum+= (int)array[i];
-                    }
-                }
-                return sum;
-            }
+            #region generic collection
+            #region  [List<T>]
+            //List<int> numbers = new List<int>();
+            //Console.WriteLine($"count:{numbers.Count} , capacty:{numbers.Capacity}");
+            //numbers.Add(1);
+            //Console.WriteLine($"count:{numbers.Count} , capacty:{numbers.Capacity}");
+            //numbers.AddRange(new int[] { 2, 3, 4, 5 });
+            //Console.WriteLine($"count:{numbers.Count} , capacty:{numbers.Capacity}");
+            //numbers.Add(6);
+            //Console.WriteLine($"count:{numbers.Count} , capacty:{numbers.Capacity}");
+            //numbers.TrimExcess();
+            //Console.WriteLine($"count:{numbers.Count} , capacty:{numbers.Capacity}");
+            //========================================
+            //List<int> numbersWithCapacity = new List<int>(5) { 1, 2, 3, 4, 5 };
+            //Console.WriteLine($"count:{numbersWithCapacity.Count} , capacty:{numbersWithCapacity.Capacity}");
+            //Console.WriteLine(SumList(numbersWithCapacity));
             #endregion
+            #endregion
+
+
             #endregion
         }
+        #region method to sum nongeneric arraylist
+        //static int SumArrayList(ArrayList array)
+        //{
+        //    int sum = 0;
+        //    if (array != null) {
+        //        for (int i=0; i< array.Count; i++) { 
+        //            sum+= (int)array[i];
+        //        }
+        //    }
+        //    return sum;
+        //}
+        #endregion
+        #region method to sum generic list 
+        static int SumList(List<int> list) 
+        {
+            int sum = 0;
+            if (list != null)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    sum += list[i];
+                }
+            }
+            return sum;
+        }
+        #endregion
     }
 }
