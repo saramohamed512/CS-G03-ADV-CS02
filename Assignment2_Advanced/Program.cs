4dabdc039af98a7687a74c151835c49e61653c4d
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-08-15 17:15:27 +0300

    Add project files.

---
+++ Assignment2_Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
